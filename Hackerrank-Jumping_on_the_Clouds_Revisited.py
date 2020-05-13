b4555f76ee936ebdee6d09a7b13f0ddc25c85aa2
ARPIT SHUKLA <47503958+arpitabhi@users.noreply.github.com>
2020-05-14 03:17:18 +0530

    Create Hackerrank-Jumping_on_the_Clouds_Revisited.py

---
+++ Hackerrank-Jumping_on_the_Clouds_Revisited.py
@@ -0,0 +1,65 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    
+    #Solution 1
+    '''
+    thun=jump=i=0
+    while True:
+        jump+=1
+        i=(i+k)%len(c)
+        if c[i]==1:
+            thun+=1
+        elif i==0:
+            break
+    return 100-jump-(thun*2)
+    '''
+    #Solution 2
+    '''
+    e=100
+    i=0
+    while (i+k)%len(c)!=0:
+        if c[(i+k)%len(c)]==1:
+            e=e-3
+        else:
+            e=e-1
+        i=(i+k)%len(c)
+    return e
+    '''
+
+    #Solution 3
+    E=99
+    i=k%len(c)
+    if c[i]==1:
+        E-=2
+    while i!=0:
+        E-=1
+        i=(i+k)%len(c)
+        if c[i]==1:
+            E-=2
+    return E
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
