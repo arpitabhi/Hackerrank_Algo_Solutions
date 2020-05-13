014361da75f8bb484fe7d8feb6bfd0c2cb3a7a2b
Arpit Shukla <arpitabhi@gmail.com>
2020-05-14 04:32:02 +0530

    Created

---
+++ Hackerrank-Find-Digits.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+#Author Arpit Shukla
+#Date 14th May, 2020
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the findDigits function below.
+def findDigits(n):
+    count=0
+    e=n
+    while(e>0):
+        t=e%10
+        e=e//10
+        if t==0:
+            pass
+        elif n%t==0:
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
