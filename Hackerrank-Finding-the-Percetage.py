52d96b3895c1954f47a3dec433f72a3d76ca81c5
Arpit Shukla <arpitabhi@gmail.com>
2020-05-21 04:45:06 +0530

    Hackerrank-Finding-the-Percetage.py

---
+++ Hackerrank-Finding-the-Percetage.py
@@ -0,0 +1,13 @@
+#Author : Arpit Shukla
+#Date : 21st May,2020
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    x=sum(student_marks[query_name])/len(student_marks[query_name])
+    print("%.2f"%x)
