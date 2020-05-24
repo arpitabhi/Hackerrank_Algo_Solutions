de43813c30bfaa62deba71124bde5d3fd878dba2
Arpit Shukla <arpitabhi@gmail.com>
2020-05-24 17:34:42 +0530

    Hackerrank String Split and Join Solution Python

---
+++ Hackerrank-String-Split-Join-Solution-Python.py
@@ -0,0 +1,13 @@
+#Author : Arpit Shukla
+# Date : 24th May, 2020
+
+# Hackerrank String Split and Join Solution Python
+
+
+def split_and_join(line):
+    # write your code here
+    line=line.split()
+    line ="-".join(line)
+    return line
+
+if __name__ == '__main__':
