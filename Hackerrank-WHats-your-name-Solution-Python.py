f74d195265464db010647588958d52d734e94b76
Arpit Shukla <arpitabhi@gmail.com>
2020-05-24 17:37:41 +0530

    Hackerrank Whats your name? Solution Python

---
+++ Hackerrank-WHats-your-name-Solution-Python.py
@@ -0,0 +1,8 @@
+#Author : Arpit Shukla
+#Date : 24th May, 2020
+# Hackerrank Whats your name? Solution Python
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a,b))
+
+if __name__ == '__main__':
