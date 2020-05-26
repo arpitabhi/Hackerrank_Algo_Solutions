1165bded40265bb8d08f33a9957fa825a6c8f346
Arpit Shukla <arpitabhi@gmail.com>
2020-05-27 01:25:17 +0530

    Added new solution

--- Hackerrank-String-Validation-Solution-Python.py
+++ Hackerrank-String-Validation-Solution-Python.py
@@ -3,8 +3,11 @@
 
 # Hackerrank String Validation Solution Python
 
+import re
 if __name__ == '__main__':
     s = input()
+    # Solution 1 : Brute Force
+    '''
     an=ac=d=l=u=0
     for i in s:
         if i.isalnum():
@@ -35,6 +38,28 @@ if __name__ == '__main__':
     else:
         print("False")
     if u>0:
+        print("True")
+    else:
+        print("False")
+    '''
+    # Solution 2: Using RegEx
+    if re.search('[a-zA-Z0-9]',s):
+        print("True")
+    else:
+        print("False")
+    if re.search('[a-zA-Z]',s):
+        print("True")
+    else:
+        print("False")
+    if re.search('[0-9]',s):
+        print("True")
+    else:
+        print("False")
+    if re.search('[a-z]',s):
+        print("True")
+    else:
+        print("False")
+    if re.search('[A-Z]',s):
         print("True")
     else:
         print("False")
